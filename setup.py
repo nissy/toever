580df5158bade79ddcdc99abdd38272962dc8e96
Yoshihiko Nishida <nishida@md387ja.local>
2014-01-03 08:48:54 +0900

    python 2.6

--- setup.py
+++ setup.py
@@ -1,4 +1,11 @@
 from setuptools import setup, find_packages
+import sys
+
+
+install_requires = ['evernote', 'clint']
+
+if sys.version_info < (2, 7):
+    install_requires.append('argparse')
 
 setup(name='everton',
       version='0.1',
@@ -7,7 +14,7 @@ setup(name='everton',
       author_email='nishida@ngc224.org',
       url='https://github.com/ngc224/everton',
       packages=find_packages(),
-      install_requires=['evernote', 'clint'],
+      install_requires=install_requires,
       entry_points="""
       [console_scripts]
       everton = everton.everton:main
